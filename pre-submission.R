3537e8752ff3c339d39b73290ec4ce2d31722758
Jindra Lacko <jindra.lacko@gmail.com>
2021-05-26 09:00:49 +0200

    v0.2.2 accepted on CRAN

--- pre-submission.R
+++ pre-submission.R
@@ -1,8 +1,8 @@
 # pre-submission routine
 
-rhub::check_on_windows()
-rhub::check_for_cran(platforms = "macos-highsierra-release-cran")
+# nepřeskakovat, důležité! encoding je sviň
 rhub::check_for_cran(platforms = "debian-clang-devel")
+# možno nahrát jedním vrzem na https://win-builder.r-project.org/upload.aspx
 devtools::check_win_release()
 devtools::check_win_devel()
 
